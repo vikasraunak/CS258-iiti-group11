45e197904909cf39404ebf48bd31aba23c226cdf
darshan tejani <darshantejani007@users.noreply.github.com>
2014-04-24 12:19:16 +0530

    A bug created during last meess up

--- admin_getImage.php
+++ admin_getImage.php
@@ -1,13 +1,6 @@
 <?php
-//require('connection.php');
+require('connection.php');
 
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "toor";
-$mysql_database = "alumni+website";
-$table="alumni";
-$con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysqli_select_db($con, $mysql_database) or die("Could not select database");
 /*
 if(mysql_ping()){
 	echo "<script type='text/javascript'>alert('Yes')</script>";
