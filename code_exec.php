8eacc069a499d255fed5901bce590325afc29872
Vikas Raunak <cse1200137@iiti.ac.in>
2015-07-26 09:26:59 +0530

    Deleted

--- code_exec.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require('connection.php');
-$username=$_POST['username'];
-$password=hash('sha256',$_POST['password']);
-mysqli_query($con, "INSERT INTO $table(username, password)VALUES('$username', '$password')");
-header("location: register.php?remarks=success");
-mysqli_close($con);
-?>
